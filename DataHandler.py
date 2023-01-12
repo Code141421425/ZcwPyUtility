ef2ed96df0143a0a9ab65b64fb1550d234b70a05
zhangchunwei <794146180@qq.com>
2023-01-12 20:07:50 +0800

    【Modify】 Split Str

--- DataHandler.py
+++ DataHandler.py
@@ -1,8 +1,16 @@
 
 
-def SplitStr(str, preSeparator, subSeparator):
+def SplitStr(str, preSeparator, subSeparator=None):
+    """
+    DO: 返回字符串截取的部分
+    """
+    # if subSeparator:
+    #     result = 
+    # else:
+    #     result = str.split(preSeparator)[1]
+    
     return str.split(preSeparator)[1].split(subSeparator)[0]
 
 def SplitStrAsFloat(str, preSeparator, subSeparator):
-    return float(str.split(preSeparator)[1].split(subSeparator)[0])
+    return float(SplitStr(str, preSeparator, subSeparator))
 
